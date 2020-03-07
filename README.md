111c536c4e21ceaa150c8c91f4ababa2a55be1ed
weiyinfu <wei.yinfu@qq.com>
2020-03-07 15:49:35 +0800

    good

---
+++ README.md
@@ -0,0 +1,8 @@
+# 在学校写的诗
+
+poem目录为使用python解析docx的代码，用来提取全部诗歌，从而可以使用HTML展示。原始数据都在“象牙塔集.docx”里面。
+
+# 程序说明
+poem目录是程序：
+* parse.py用于解析docx文档，生成poem.json
+* index.html用于展示poem.json
---
+++ poem/parse.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+import docx
+
+"""
+解析若干个heading之间的内容作为诗歌内容
+第一个Heading之前的内容是目录和序
+"""
+
+
+def show(paragraph):
+    # 展示一个变量的非私有属性
+    for i in dir(paragraph):
+        if i.startswith("_") or i.startswith("add"):
+            continue
+        try:
+            value = getattr(paragraph, i)
+            print(i, value)
+        except:
+            pass
+
+
+doc_path = os.path.join("..", "象牙塔集.docx")
+doc = docx.Document(doc_path)
+poem_list = []
+now_poem = {}
+for p in doc.paragraphs:
+    print("=", p.text, p.style.name, p.style)
+    if p.style.name == "Heading 1":
+        if now_poem:
+            poem_list.append(now_poem)
+            now_poem['content'] = '\n'.join(now_poem['content'])
+            now_poem = {}
+        now_poem['title'] = p.text.strip()
+        now_poem['content'] = []
+    elif p.style.name == "Normal" and now_poem:
+        now_poem['content'].append(p.text)
+if now_poem.keys():
+    now_poem['content'] = '\n'.join(now_poem['content'])
+    poem_list.append(now_poem)
+print(poem_list)
+json.dump(poem_list, open("poem.json", "w", encoding='utf8'), ensure_ascii=0, indent=2)
